a420b8bfb9c548a54387ad1635f9e9294ad3eb77
Stefan Ledin <stefan.ledin@gmail.com>
2017-10-23 06:38:31 +0200

    vue components

--- resources/assets/js/components/entry.vue
+++ resources/assets/js/components/entry.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card bg-secondary text-light mb-4">
         <div class="card-header">
-            <h4 class="card-title float-left"></h4>
+            <h4 class="card-title float-left">00:00:00</h4>
             <button class="btn btn-dark float-right" v-if="ended_at" v-on:click="resume">Fortsätt</button>
             <button class="btn btn-danger float-right" v-else v-on:click="stop">Stopp</button>
         </div>
@@ -21,59 +21,67 @@
 </template>
 
 <script>
+    const Timer = require('easytimer');
+
     export default {
 
         props:['entry'],
 
         data() {
-            return {
-                id: this.entry.id,
-                working_with: this.entry.working_with,
-                duration: this.entry.duration,
-                ended_at: this.entry.ended_at
-            }
+            return this.entry;
+        },
+
+        mounted() {
+            const h4 = this.$el.querySelector('h4');
+            const timer = new Timer();
+            timer.start();
+            timer.addEventListener('secondsUpdated', function (event) {
+                 h4.innerHTML = timer.getTimeValues().toString();
+            });
         },
 
         methods: {
             decrease: function () {
-                if (this.entry.duration > 0) {
-                    this.entry.duration -= 0.25
+                if (this.duration > 0) {
+                    this.duration -= 0.25
                 }
                 this.sync();
             },
 
             increase: function () {
-                this.entry.duration += 0.25;
+                this.duration += 0.25;
+                this.sync();
             },
 
             sync() {
                 axios.post(`/entry/${this.id}/stop`, {
-                    working_with: this.working_with
-                })
-                    .then(function (response) {
-                        console.log(response);
-                    })
-                    .catch((error) => console.log(error));
+                    working_with: this.working_with,
+                    duration: this.duration
+                });
             },
 
             resume() {
-                axios.post(`/entry/${this.id}/resume`)
-                    .then(function (response) {
-                        this.working_with = response.data.working_with;
-                        this.duration = response.data.duration;
-                        this.ended_at = response.data.ended_at;
-                    })
+                axios.post(`/entry/${this.id}/resume`, {
+                    working_with: this.working_with,
+                    duration: this.duration
+                })
+                    .then(this.update.bind(this))
                     .catch((error) => console.log(error));
             },
 
             stop() {
-                axios.post(`/entry/${this.id}/stop`)
-                    .then(function (response) {
-                        this.working_with = response.data.working_with;
-                        this.duration = response.data.duration;
-                        this.ended_at = response.data.ended_at;
-                    })
+                axios.post(`/entry/${this.id}/stop`, {
+                    working_with: this.working_with,
+                    duration: this.duration
+                })
+                    .then(this.update.bind(this))
                     .catch((error) => console.log(error));
+            },
+
+            update(response) {
+                this.duration = response.data.duration;
+                this.working_with = response.data.working_with;
+                this.ended_at = response.data.ended_at;
             }
         },
 
